35c94b80ac52d08c7339f7f0791d653caa120f12
Rogi88 <igor.peric777@gmail.com>
2019-03-18 09:45:26 -0400

    added analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.middleware.use(Rack::Tracker) do
+    handler :google_analytics, { tracker: 'UA-136465070-1' }
+  end
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
